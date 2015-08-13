1fc91839a212454a22d815c414702fcd99dceb9a
kazblox (mixmaster) <mixmaster@8chan.co>
2015-08-13 17:23:58 -0400

    Update sound_sn76489.c

--- PCem/sound_sn76489.c
+++ PCem/sound_sn76489.c
@@ -1,5 +1,6 @@
 /*
  *  Copyright (C) 2002-2013  The DOSBox Team
+ *  Adapted to PCem by Tom Walker
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
